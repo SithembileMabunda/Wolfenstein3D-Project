467f239d05e672640277cd19492f0bd11da4fb28
Sithembile <39857574+SithembileMabunda@users.noreply.github.com>
2019-02-26 14:21:53 +0200

    Add files via upload

--- ft_drawer.c
+++ ft_drawer.c
@@ -6,21 +6,18 @@
 /*   By: smabunda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/16 13:42:28 by smabunda          #+#    #+#             */
-/*   Updated: 2019/02/26 14:18:02 by smabunda         ###   ########.fr       */
+/*   Updated: 2019/02/26 14:20:50 by smabunda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
 
-//void	put(int x, int y, void img_ptr, int size, int bpp, t_wolf *wolf)
-void	put(int x, int y, void *img_ptr, int size, int bpp, t_wolf *wolf)
+void	put(int x, int y, void *img_ptr, t_wolf *wolf)
 {
 	if (x >= 0 && x < WINW && y >= 0 && y < WINH)
 	{
 		*(unsigned int*)(img_ptr + \
-				(bpp * x + y * size)) = wolf->color;
-		//*(unsigned int*)(img_ptr + 
-//				(wolf->bpp * x + y * wolf->size)) = wolf->color;
+				(wolf->bpp * x + y * wolf->size)) = wolf->color;
 	}
 }
 
@@ -39,9 +36,7 @@ void	drawimg(t_wolf *wolf, int x1)
 	i = 0;
 	while (i <= wolf->steps)
 	{
-		//put(wolf->x, wolf->y, wolf->img_ptr, wolf);
-		put(wolf->x, wolf->y, wolf->img, wolf->size, wolf->bpp, wolf);
-		//put(wolf->x, wolf->y, wolf->img_ptr, wolf);
+		put(wolf->x, wolf->y, wolf->img, wolf);
 		wolf->x += wolf->xx;
 		wolf->y += wolf->yy;
 		i++;
